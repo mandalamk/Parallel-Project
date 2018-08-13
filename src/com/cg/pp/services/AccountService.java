6d362b776e1ae7adeae89d1b8a203be0b699ec24
mandalamk <42267758+mandalamk@users.noreply.github.com>
2018-08-13 12:58:43 +0530

    Update AccountService.java
    Changed myexception to accountexception.

--- src/com/cg/pp/services/AccountService.java
+++ src/com/cg/pp/services/AccountService.java
@@ -6,9 +6,9 @@ import com.cg.pp.exceptions.MyException;
 
 public interface AccountService {
 
-	int showBalance(int accNum) throws MyException;
-	int deposit(int accNum, int amount) throws MyException;
-	int withdraw(int accNum, int amount) throws MyException;
-	ArrayList<Integer> transfer(int accNum_from,int accNum_to, int amount) throws MyException;
-	void printTransactions(int accNum) throws MyException;
+	int showBalance(int accNum) throws AccountException;
+	int deposit(int accNum, int amount) throws AccountException;
+	int withdraw(int accNum, int amount) throws AccountException;
+	ArrayList<Integer> transfer(int accNum_from,int accNum_to, int amount) throws AccountException;
+	void printTransactions(int accNum) throws AccountException;
 }
